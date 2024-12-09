9e4eb824d5c11ea7215e096878f42de87e0b8f4c
papp11gergely13 <90318540+papp11gergely13@users.noreply.github.com>
2024-12-09 09:24:42 +0100

    Add files via upload

---
+++ paintApp/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,344 @@
+﻿
+namespace PaintApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.BtnEraser = new System.Windows.Forms.Button();
+            this.BtnPaint = new System.Windows.Forms.Button();
+            this.BtnSave = new System.Windows.Forms.Button();
+            this.BtnErase = new System.Windows.Forms.Button();
+            this.BtnRectangle = new System.Windows.Forms.Button();
+            this.BtnElipse = new System.Windows.Forms.Button();
+            this.BtnLine = new System.Windows.Forms.Button();
+            this.BtnPencil = new System.Windows.Forms.Button();
+            this.BtnColor = new System.Windows.Forms.Button();
+            this.Pic = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.numericUpDown2);
+            this.panel1.Controls.Add(this.numericUpDown1);
+            this.panel1.Controls.Add(this.BtnEraser);
+            this.panel1.Controls.Add(this.BtnPaint);
+            this.panel1.Controls.Add(this.BtnSave);
+            this.panel1.Controls.Add(this.BtnErase);
+            this.panel1.Controls.Add(this.BtnRectangle);
+            this.panel1.Controls.Add(this.BtnElipse);
+            this.panel1.Controls.Add(this.BtnLine);
+            this.panel1.Controls.Add(this.BtnPencil);
+            this.panel1.Controls.Add(this.BtnColor);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(798, 68);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(582, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(59, 56);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Airbrush";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "ecset";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Vonal";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(63, 42);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(54, 20);
+            this.numericUpDown2.TabIndex = 10;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(63, 6);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(56, 20);
+            this.numericUpDown1.TabIndex = 9;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // BtnEraser
+            // 
+            this.BtnEraser.Location = new System.Drawing.Point(517, 6);
+            this.BtnEraser.Name = "BtnEraser";
+            this.BtnEraser.Size = new System.Drawing.Size(59, 56);
+            this.BtnEraser.TabIndex = 8;
+            this.BtnEraser.Text = "Radír";
+            this.BtnEraser.UseVisualStyleBackColor = true;
+            this.BtnEraser.Click += new System.EventHandler(this.BtnEraser_Click);
+            // 
+            // BtnPaint
+            // 
+            this.BtnPaint.Location = new System.Drawing.Point(452, 6);
+            this.BtnPaint.Name = "BtnPaint";
+            this.BtnPaint.Size = new System.Drawing.Size(59, 56);
+            this.BtnPaint.TabIndex = 7;
+            this.BtnPaint.Text = "Kitöétés";
+            this.BtnPaint.UseVisualStyleBackColor = true;
+            this.BtnPaint.Click += new System.EventHandler(this.BtnPaint_Click);
+            // 
+            // BtnSave
+            // 
+            this.BtnSave.Location = new System.Drawing.Point(729, 6);
+            this.BtnSave.Name = "BtnSave";
+            this.BtnSave.Size = new System.Drawing.Size(59, 56);
+            this.BtnSave.TabIndex = 6;
+            this.BtnSave.Text = "Kép mentése";
+            this.BtnSave.UseVisualStyleBackColor = true;
+            this.BtnSave.Click += new System.EventHandler(this.BtnSave_Click);
+            // 
+            // BtnErase
+            // 
+            this.BtnErase.Location = new System.Drawing.Point(664, 6);
+            this.BtnErase.Name = "BtnErase";
+            this.BtnErase.Size = new System.Drawing.Size(59, 56);
+            this.BtnErase.TabIndex = 5;
+            this.BtnErase.Text = "Vászon Törlés";
+            this.BtnErase.UseVisualStyleBackColor = true;
+            this.BtnErase.Click += new System.EventHandler(this.BtnClear_Click);
+            // 
+            // BtnRectangle
+            // 
+            this.BtnRectangle.Location = new System.Drawing.Point(387, 6);
+            this.BtnRectangle.Name = "BtnRectangle";
+            this.BtnRectangle.Size = new System.Drawing.Size(59, 56);
+            this.BtnRectangle.TabIndex = 4;
+            this.BtnRectangle.Text = "Négyszög";
+            this.BtnRectangle.UseVisualStyleBackColor = true;
+            this.BtnRectangle.Click += new System.EventHandler(this.BtnRectangle_Click);
+            // 
+            // BtnElipse
+            // 
+            this.BtnElipse.Location = new System.Drawing.Point(322, 6);
+            this.BtnElipse.Name = "BtnElipse";
+            this.BtnElipse.Size = new System.Drawing.Size(59, 56);
+            this.BtnElipse.TabIndex = 3;
+            this.BtnElipse.Text = "Elipszis";
+            this.BtnElipse.UseVisualStyleBackColor = true;
+            this.BtnElipse.Click += new System.EventHandler(this.BtnEllipse_Click);
+            // 
+            // BtnLine
+            // 
+            this.BtnLine.Location = new System.Drawing.Point(257, 6);
+            this.BtnLine.Name = "BtnLine";
+            this.BtnLine.Size = new System.Drawing.Size(59, 56);
+            this.BtnLine.TabIndex = 2;
+            this.BtnLine.Text = "Vonal";
+            this.BtnLine.UseVisualStyleBackColor = true;
+            this.BtnLine.Click += new System.EventHandler(this.BtnLine_Click);
+            // 
+            // BtnPencil
+            // 
+            this.BtnPencil.Location = new System.Drawing.Point(192, 6);
+            this.BtnPencil.Name = "BtnPencil";
+            this.BtnPencil.Size = new System.Drawing.Size(59, 56);
+            this.BtnPencil.TabIndex = 1;
+            this.BtnPencil.Text = "Ceruza";
+            this.BtnPencil.UseVisualStyleBackColor = true;
+            this.BtnPencil.Click += new System.EventHandler(this.BtnPencil_Click);
+            // 
+            // BtnColor
+            // 
+            this.BtnColor.Location = new System.Drawing.Point(127, 6);
+            this.BtnColor.Margin = new System.Windows.Forms.Padding(5);
+            this.BtnColor.Name = "BtnColor";
+            this.BtnColor.Padding = new System.Windows.Forms.Padding(2);
+            this.BtnColor.Size = new System.Drawing.Size(59, 56);
+            this.BtnColor.TabIndex = 0;
+            this.BtnColor.Text = "Eszköz színe";
+            this.BtnColor.UseVisualStyleBackColor = true;
+            this.BtnColor.Click += new System.EventHandler(this.BtnColor_Click);
+            // 
+            // Pic
+            // 
+            this.Pic.BackColor = System.Drawing.Color.White;
+            this.Pic.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.Pic.Location = new System.Drawing.Point(0, 135);
+            this.Pic.Name = "Pic";
+            this.Pic.Size = new System.Drawing.Size(798, 382);
+            this.Pic.TabIndex = 1;
+            this.Pic.TabStop = false;
+            this.Pic.Paint += new System.Windows.Forms.PaintEventHandler(this.Pic_Paint);
+            this.Pic.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Pic_MouseClick);
+            this.Pic.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Pic_MouseDown);
+            this.Pic.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Pic_MouseMove);
+            this.Pic.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Pic_MouseUp);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Egér X:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 102);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Egér Y:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(82, 80);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(13, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(82, 102);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(13, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(798, 517);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Pic);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox Pic;
+        private System.Windows.Forms.Button BtnColor;
+        private System.Windows.Forms.Button BtnSave;
+        private System.Windows.Forms.Button BtnErase;
+        private System.Windows.Forms.Button BtnRectangle;
+        private System.Windows.Forms.Button BtnElipse;
+        private System.Windows.Forms.Button BtnLine;
+        private System.Windows.Forms.Button BtnPencil;
+        private System.Windows.Forms.Button BtnPaint;
+        private System.Windows.Forms.Button BtnEraser;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ paintApp/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace PaintApp
+{
+    public partial class Form1 : Form
+    {    
+        Bitmap bm; //Declarálni kell, mert arajzolatot el kell menteni
+        Graphics g; // Itt vannak a grafikai eszközök
+        bool paint = false; // Szabad e rajzolni
+        Point px, py; // átmeneti pont koordináták
+        Pen p = new Pen(Color.Black, 1); // Eszköz a rajzoláshoz
+        Pen Eraser = new Pen(Color.White, 10); 
+        int DrawTool = 1; // A rajzolás típusa
+        int x, y, sx, sy, cx, cy; // átmeneti koordináták
+        // bool saved = false;
+        Color New_Color; // Rajzolás színe
+        ColorDialog cd = new ColorDialog(); // Színválaszték
+       
+        public Form1()
+        {
+            InitializeComponent();
+            bm = new Bitmap(Pic.Width, Pic.Height); // Bimap példány
+            g = Graphics.FromImage(bm); //új rajzot hoz létre képből (bm)
+            g.Clear(Color.White); // tőrlés: háttérszínnel kitöltés
+            Pic.Image = bm; // Image mentésa a paintboxból
+            p.Width = (float)numericUpDown1.Value; //rajzoló eszköz vastagsága
+        }
+
+        private void BtnPencil_Click(object sender, EventArgs e)
+        {
+            DrawTool = 1; //Rajzeszköz ceruza
+        }
+
+        private void Pic_MouseDown(object sender, MouseEventArgs e)
+        {
+            
+            paint = true; // engedélyezem a rajzolást
+            py = e.Location; //elmentem az egérkurzor aktuális állapotát
+            cx = e.X; 
+            cy = e.Y;
+            label5.Text = Convert.ToString(e.Location);
+            label6.Text = Convert.ToString(e.Location);
+        }
+
+        private void Pic_MouseMove(object sender, MouseEventArgs e)
+        {
+            label5.Text = Convert.ToString(px);
+            label6.Text = Convert.ToString(py);
+            if (paint) //ha rajzolás közben mozog a kurzor
+            {
+                if (DrawTool == 1) //ha eszköz ceruza
+                {
+                    px = e.Location; //elmentem az egérkurzor új koordinátáit
+                    g.DrawLine(p, px, py); //rövidke vonalat húzok
+                    py = px; //egér startkoordináta csere
+                }
+               if (DrawTool == 2)  //ha eszköz radír, mint 1
+                {
+                    px = e.Location;
+                    g.DrawLine(Eraser, px, py);
+                    py = px;
+                }
+                if (DrawTool == 7)
+                {
+                    px = e.Location;                    
+                    g.DrawEllipse(p, px.X , py.Y , (float)numericUpDown2.Value, (float) numericUpDown2.Value);
+                    g.DrawEllipse(Eraser, px.X, py.Y, (float)numericUpDown2.Value-2, (float)numericUpDown2.Value-2);
+                    py = px;
+                }
+            }
+            Pic.Refresh();
+            x = e.X;
+            y = e.Y;
+            sx = e.X - cx;
+            sy = e.Y - cy;
+        }
+
+        private void Pic_MouseUp(object sender, MouseEventArgs e)
+        {
+            paint = false;
+            sx = x - cx;
+            sy = y - cy;
+            if (DrawTool == 3)
+            {
+                g.DrawEllipse(p, cx, cy, sx, sy);
+            }
+            if (DrawTool == 4)
+            {
+                g.DrawRectangle(p, cx, cy, sx, sy);
+            }
+            if (DrawTool == 5)
+            {
+                g.DrawLine(p, cx, cy, x, y);
+            }
+        }
+
+        private void BtnEraser_Click(object sender, EventArgs e)
+        {
+            DrawTool = 2; //Rajzeszköz radír
+        }
+
+        private void BtnEllipse_Click(object sender, EventArgs e)
+        {
+            DrawTool = 3;  //Rajzeszköz elipszis, spec kör
+        }
+
+        private void BtnRectangle_Click(object sender, EventArgs e)
+        {
+            DrawTool = 4;  //Rajzeszköz négyzet
+        }
+
+        private void BtnLine_Click(object sender, EventArgs e)
+        {
+            DrawTool = 5;  //Rajzeszköz vonalhúzás
+        }
+
+        private void Pic_Paint(object sender, PaintEventArgs e)
+        {
+            //Kirajzolása azalakzatnak
+            Graphics g = e.Graphics;
+            if (paint)
+            {
+                if (DrawTool == 3)
+                {
+                    g.DrawEllipse(p, cx, cy, sx, sy);
+                }
+                if (DrawTool == 4)
+                {
+                    g.DrawRectangle(p, cx, cy, sx, sy);
+                }
+                if (DrawTool == 5)
+                {
+                    g.DrawLine(p, cx, cy, x, y);
+                }
+                if (DrawTool == 7)
+                {
+                        //megírni
+                }
+            }
+        }
+
+       
+
+        private void BtnClear_Click(object sender, EventArgs e)
+        {
+            // Vászon törlése
+            g.Clear(Color.White);
+            Pic.Image = bm;
+            DrawTool = 0;
+        }
+
+        private void BtnPaint_Click(object sender, EventArgs e)
+        {
+            DrawTool = 6; // Zárt alakzat kifestése eszköz
+        }
+
+ 
+       
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            // Eszköz vonalvastagsága
+            p.Width = (float)numericUpDown1.Value;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DrawTool = 7;  //Rajzeszköz szórópisztoly
+        }
+
+       
+
+
+        static Point set_Point(PictureBox pb, Point pt)
+        {
+            // Zárt alakzat keresése a kitöltéséhez algoritmus
+            float px = 1f * pb.Width / pb.Width;
+            float py = 1f * pb.Height / pb.Height;
+            return new Point((int)(pt.X * px), (int)(pt.Y * py));
+        }
+        private void Validate(Bitmap bm, Stack<Point> sp, int x, int y, Color Old_Color, Color New_Color)
+        {  
+            // Zárt alakzat keresése a kitöltéséhez algoritmus
+            Color cx = bm.GetPixel(x, y);
+            if (cx == Old_Color)
+            {
+                sp.Push(new Point(x, y));
+                bm.SetPixel(x, y, New_Color);
+            }
+        }
+
+        public void Fill(Bitmap bm, int x, int y, Color New_Clr)
+        {
+            // Zárt alakzat kitöltéséhez algoritmus
+            Color Old_Color = bm.GetPixel(x, y);
+            Stack<Point> pixel = new Stack<Point>();
+            pixel.Push(new Point(x, y));
+            bm.SetPixel(x, y, New_Clr);
+            if (Old_Color == New_Clr) { return; }
+
+            while (pixel.Count > 0)
+            {
+                Point pt = (Point)pixel.Pop();
+                if (pt.X > 0 && pt.Y > 0 && pt.X < bm.Width - 1 && pt.Y < bm.Height - 1)
+                {
+                    Validate(bm, pixel, pt.X - 1, pt.Y, Old_Color, New_Clr);
+                    Validate(bm, pixel, pt.X, pt.Y - 1, Old_Color, New_Clr);
+                    Validate(bm, pixel, pt.X + 1, pt.Y, Old_Color, New_Clr);
+                    Validate(bm, pixel, pt.X, pt.Y + 1, Old_Color, New_Clr);
+                }
+            }
+        }
+
+        private void Pic_MouseClick(object sender, MouseEventArgs e)
+        {
+            // Zárt alakzat kitöltése
+
+            if (DrawTool == 6)
+            {
+                Point point = set_Point(Pic, e.Location);
+                Fill(bm, point.X, point.Y, New_Color);
+            }
+        }
+
+        private void BtnColor_Click(object sender, EventArgs e)
+        {
+            // //Rajzeszköz színének megváltoztatása
+            cd.ShowDialog();
+            New_Color = cd.Color;
+            Pic.BackColor = cd.Color;
+            p.Color = cd.Color;
+
+        }
+
+        private void BtnSave_Click(object sender, EventArgs e)
+        {
+            //Kép mentése
+            var sfd = new SaveFileDialog();
+            sfd.Filter = "Image(*.jpg)|*.jpg|(*.*)|*.*";
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                Bitmap btm = bm.Clone(new Rectangle(0, 0, Pic.Width, Pic.Height), bm.PixelFormat);
+                btm.Save(sfd.FileName, ImageFormat.Jpeg);
+            }
+        }
+    }
+}
+
